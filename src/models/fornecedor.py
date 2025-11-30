1a9700723904bdbbaed9f1414ca859debfde15b0
KpSantiago <santiago.kauajose11@gmail.com>
2025-11-30 17:17:33 -0300

    adicionar nome de cada tabela e mudar o tipo de dado da coluna data_transacao

--- src/models/fornecedor.py
+++ src/models/fornecedor.py
@@ -3,6 +3,7 @@ from typing import List
 
 
 class Fornecedor(SQLModel, table=True):
+    __tablename__ = "fornecedor"
     idForn: int | None = Field(default=None, primary_key=True)
     cnpj: int
     nome: str
