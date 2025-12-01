fe2e05076f70f4cae1d5a7de8c93091ea095100e
KpSantiago <santiago.kauajose11@gmail.com>
2025-11-30 22:42:00 -0300

    adicionar tablename e adicionar valor pra produto transacao forncedor

--- src/models/ProdutoTransacaoFornecedor.py
+++ src/models/ProdutoTransacaoFornecedor.py
@@ -4,11 +4,11 @@ from .fornecedor import Fornecedor
 from .transacao import Transacao, TransacaoResponse
 
 class ProdutoTransacaoFornecedor(SQLModel, table=True):
-    __tablename__ = "produto_transacao_fornecedor"
     produto_id: int = Field(foreign_key="produto.idProd", primary_key=True)
     fornecedor_id: int = Field(foreign_key="fornecedor.idForn", primary_key=True)
     transacao_id: int = Field(foreign_key="transacao.transacao_id", primary_key=True)
     quantidade: int
+    valor: float
     
     produto: "Produto" = Relationship(back_populates="transacoesProduto")
     fornecedor: "Fornecedor" = Relationship(back_populates="transacoesFornecedor")
--- src/models/transacao.py
+++ src/models/transacao.py
@@ -2,7 +2,6 @@ from sqlmodel import Field, SQLModel, Relationship
 from datetime import datetime;
 
 class Transacao(SQLModel, table=True):
-    
     transacao_id: int | None = Field(default=None, primary_key=True)
     quantidade: int 
     data_transacao: datetime | None = Field(default=None)
